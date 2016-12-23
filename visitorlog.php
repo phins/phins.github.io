80e133bfec8a9edc1e693b12b3d414a1e3c9d8cd
phins <phiny91@gmail.com>
2016-12-23 08:44:49 -0800

    Add files via upload

---
+++ visitorlog.php
@@ -0,0 +1,37 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Brand</a>
+    </div>
+    <div class="collapse navbar-collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+            <li><a href="visitorlog.php">Visitor Log</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="current.html">Current Projects</a></li>
+            <li><a href="https://docs.google.com/document/d/1qZ-6dkPmtvw270681NmIoHU-1xFZTD68aGH6ByoQ1b8/edit?usp=sharing">My Resume</a></li>
+      </ul>
+    </div><!--/.nav-collapse -->
+  </div>
+</div>
+
+<div class="container">
+  
+  <div class="text-center">
+    <h1>Visitor Log</h1>
+    <p class="lead">Welcome to my site!</p>
+    <p>Below, log in your visit by enter a quote and your name!</p>
+  </div>
+  <form action="welcome.php" method="post">
+Enter your Quote: <input type="text" name="name"><br>
+Your name: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+  
+</div><!-- /.container -->
